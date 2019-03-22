5f7de5da2fbb3dd7a31a67ce89339cee6ab7cdf4
kosten27@gmail.com <kosten27@gmail.com>
2019-03-22 20:33:52 +0200

    Added Spring Controller

--- src/main/java/com/kostenko/controllers/ClientController.java
+++ src/main/java/com/kostenko/controllers/ClientController.java
@@ -1,23 +1,32 @@
 package com.kostenko.controllers;
 
+import com.kostenko.dao.DataSourceDB;
+import com.kostenko.dao.impl.ClientDBDao;
 import com.kostenko.services.ClientService;
+import com.kostenko.services.impl.ClientServiceImpl;
+import com.kostenko.validators.impl.ValidationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/clients2")
 public class ClientController {
+//    public static final ClientDBDao CLIENT_DAO = new ClientDBDao(new DataSourceDB());
     @Autowired
+//    private ClientService clientService = new ClientServiceImpl(CLIENT_DAO, new ValidationServiceImpl(CLIENT_DAO));
     private ClientService clientService;
 
-    @GetMapping("/clients2")
+    @GetMapping()
     public String showClients(ModelMap modelMap){
         System.out.println("showClients");
         modelMap.put("message", clientService.getAllClients());
         return "clients";
+    }
+
+    @PostMapping()
+    public void postClient(@RequestParam String name, @RequestParam String surname, @RequestParam String phone) {
+        clientService.createClient(name, surname, phone);
     }
 }
