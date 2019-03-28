0109bbec06e83b668b682a6acdef905ecd0d9c93
kosten27 <kosten27@gmail.com>
2019-03-28 23:56:41 +0200

    Added Hibernate

--- src/main/java/com/kostenko/App.java
+++ src/main/java/com/kostenko/App.java
@@ -1,28 +1,9 @@
 package com.kostenko;
 
-import com.kostenko.dao.ClientDao;
-import com.kostenko.dao.DataSourceDB;
-import com.kostenko.dao.OrderDao;
-import com.kostenko.dao.ProductDao;
-import com.kostenko.dao.impl.*;
-import com.kostenko.services.ClientService;
-import com.kostenko.services.OrderService;
-import com.kostenko.services.ProductService;
-import com.kostenko.services.impl.ClientServiceImpl;
-import com.kostenko.services.impl.OrderServiceImpl;
-import com.kostenko.services.impl.ProductServiceImpl;
-import com.kostenko.validators.ValidationService;
-import com.kostenko.validators.impl.ValidationServiceImpl;
-import com.kostenko.view.AdminMenu;
-import com.kostenko.view.ClientMenu;
 import com.kostenko.view.MainMenu;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class App {
 
